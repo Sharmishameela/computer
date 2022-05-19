2f900fa6395dc1e3483caeb1bd5514f5edcc71c9
Sharmishameela <sharmishameela@gmail.com>
2022-05-19 21:43:05 +0530

    saved locally

---
+++ src/Desktop.java
@@ -0,0 +1,17 @@
+
+public class Desktop extends Computer{
+	
+	public void desktopSize()
+	{
+		System.out.println("64bit");
+	}
+	public static void main(String[] args) {
+		
+	Desktop d=new Desktop();
+	d.computerModel();
+	d.desktopSize();
+	
+	}
+	
+
+}
